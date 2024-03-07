168baeca4128286c66592006a603c8e2c1f830e7
k8s-1pro <k8s.1pro@gmail.com>
2024-03-07 12:41:23 +0900

    롬복 제거

--- src/main/java/com/pro/app/domain/DatasourceProperties.java
+++ src/main/java/com/pro/app/domain/DatasourceProperties.java
@@ -1,10 +1,7 @@
 package com.pro.app.domain;
 
-import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-
-
 
 @Configuration
 @ConfigurationProperties("datasource")
